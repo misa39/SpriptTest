e2724e111317fea54c7fdd11f7c8def97d33088d
misa <rararala7@gmail.com>
2017-04-29 22:58:39 +0900

    ScriptTest
    課題

---
+++ Assets/BossTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTest : MonoBehaviour {
+
+		void Start () {
+				int MP = 53;
+				if (MP >= 5) {
+					Debug.Log("魔法攻撃をした。残りMPは〇〇");
+				} else if( MP >=0) {
+						Debug.Log("MPが足りないため魔法が使えない。");
+		}
+				
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {
+		private int MP = 53;
+		private int Magic = 5;
+
+		public void Attack() {
+		Debug.Log (this.Magic + "魔法攻撃をした。残りMPは");
+		}
+
+		public void Defence(int damage) {
+				Debug.Log (damage + "のダメージを受けた");
+				this.MP -= damage;
+		}
+		
+}
+
+public class Test : MonoBehaviour {
+	
+
+		void Start () {
+				Boss lastboss = new Boss ();
+
+				lastboss.Attack ();
+				lastboss.Defence (3);
+		}
+
+		void Update () {
+		
+		}
+}
